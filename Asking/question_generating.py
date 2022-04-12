7754e21742e9eb72d68fad182ee708e775d53597
ZhuoranLiu123 <zliu3@andrew.cmu.edu>
2022-04-12 00:54:33 -0400

    modify ask format

--- Asking/question_generating.py
+++ Asking/question_generating.py
@@ -2,18 +2,20 @@ from tag import *
 import stanza
 import logging
 
+
 def match_npvp(tree):
     tree = tree.children[0]
     if tree.label != SENTENCE:
         return False
     if len(tree.children) != 3:
         return False
-    #look for np and vp structure
+    # look for np and vp structure
     if tree.children[0].label != NP:
         return False
     if tree.children[1].label != VP:
         return False
     return True
+
 
 def binary_questions(doc):
     question = ""
@@ -36,6 +38,7 @@ def binary_questions(doc):
 
     return question
 
+
 def aux_binary_quesitons(doc):
     question = ""
     for word in doc.sentences[0].words:
@@ -48,31 +51,35 @@ def aux_binary_quesitons(doc):
 
     return question
 
+
 def ner_questions(doc, sentence):
     questions = []
-    ents = {}
-    for ent in doc.sentences[0].ents:
-        ents[ent.text] = ent.type
-    words = sentence.split()
-    first_word = words[0]
-    if first_word in ents.keys():
-        words[0] = get_wh(ents[first_word])
-        if words[0] is not None:
-            question = " ".join(words)
-            questions.append(format_question(question))
-            del ents[first_word]
-            return questions
+    try:
+        ents = {}
+        for ent in doc.sentences[0].ents:
+            ents[ent.text] = ent.type
+        words = sentence.split()
+        first_word = words[0]
+        if first_word in ents.keys():
+            words[0] = get_wh(ents[first_word])
+            if words[0] is not None:
+                question = " ".join(words)
+                questions.append(format_question(question))
+                del ents[first_word]
+                return questions
 
-    base = binary_questions(doc)
-    # print("base:", base)
-    for ent in ents.keys():
-        wh = get_wh(ents[ent])
-        if wh is None:
-            continue
-        # print("wh:", wh)
-        question = wh + " " + base
-        question = question.replace(ent, "", 1)
-        questions.append(format_question(question))
+        base = binary_questions(doc)
+        # print("base:", base)
+        for ent in ents.keys():
+            wh = get_wh(ents[ent])
+            if wh is None:
+                continue
+            # print("wh:", wh)
+            question = wh + " " + base
+            question = question.replace(ent, "", 1)
+            questions.append(format_question(question))
+    except:
+        return []
     return questions
 
 
@@ -83,6 +90,7 @@ def get_wh(ent):
         return "where"
     if ent == "DATE":
         return "when"
+
 
 def why_questions(doc):
     question = binary_questions(doc)
@@ -97,6 +105,7 @@ def format_question(question):
     str = str[0].upper() + str[1:len(str)]
     str = str + "?"
     return str
+
 
 def generating(sentences):
     logger = logging.getLogger('stanza')
@@ -125,10 +134,13 @@ def generating(sentences):
     logger.disabled = False
     return binary, wh
 
+
 # Main program
 if __name__ == "__main__":
-    sentences = ["John made a cake.", "Mary makes a cake.", "I make a cake.", "John has made a cake.", "I have made a cake.", "She had made a cake.", "David had lunch in New York with Mary last Sunday because they did not meet in 10 years."]
-    #stanza.download(lang='en', processors='tokenize,mwt,pos,constituency,lemma,depparse, ner')
+    sentences = ["John made a cake.", "Mary makes a cake.", "I make a cake.", "John has made a cake.",
+                 "I have made a cake.", "She had made a cake.",
+                 "David had lunch in New York with Mary last Sunday because they did not meet in 10 years."]
+    # stanza.download(lang='en', processors='tokenize,mwt,pos,constituency,lemma,depparse, ner')
     # nlp = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,constituency,lemma,depparse, ner')
     # questions = []
     # for line in sentences:
@@ -153,6 +165,3 @@ if __name__ == "__main__":
     questions = generating(sentences)
 
     print(questions)
-
-
-
